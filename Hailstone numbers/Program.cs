7e3f14491b354d218416b01b924b4524b2026390
uljas <uljas@DESKTOP-8KIG1O2>
2021-10-10 17:24:53 +0200

    Add project files.

---
+++ Hailstone numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Hailstone_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            while (true)
+            {
+                Console.WriteLine("Write STOP for brake");
+                Console.WriteLine("Enter int:");
+
+                string input = Console.ReadLine();
+                
+                while (int.TryParse(input, out num))
+                {
+                    if (input == "STOP") break;
+                    while(true)
+                    {
+                        
+                        
+                            if (num % 2 == 0)
+                            {
+                                num = num / 2;
+                            }
+                            else
+                            {
+                                num = num * 3;
+                                num = num + 1;
+                            }
+
+                        System.Threading.Thread.Sleep(130);
+                        Console.WriteLine(num);
+                        if (num == 1) break;
+                    }
+                    break;
+
+                }
+                Console.WriteLine("Type 1 for close enter for restart");
+                string closer = Console.ReadLine();
+                if (closer == "1")
+                {
+                    break;
+                }
+                Console.Clear();
+                
+
+            }
+
+        }
+    }
+}
